434891727d63974c71e0dc9fb85f9c6d19c47d1b
source-guilherme <guilherme_goi@hotmail.com>
2021-07-06 21:08:58 -0400

    Got smallMaker rotation to 0

--- RotateMinimapMod/RotateMinimapMod.cs
+++ RotateMinimapMod/RotateMinimapMod.cs
@@ -13,21 +13,26 @@ namespace RotateMinimapMod
         public const string PluginGUID = "com.source-guilherme.rotateminimapmod";
         public const string PluginName = "RotateMinimapMod";
         public const string PluginVersion = "0.0.1";
-        //private ConfigEntry<float> FloatConfig;
 
         private void Awake()
         {
-            //FloatConfig = Config.Bind<float>("Minimap Zoom", "Zoom Values", 1f, new ConfigDescription("This will change the Field of View in the game.", new AcceptableValueRange<float>(0f, 2f)));
             On.Minimap.CenterMap += Minimap_CenterMap;
             On.Minimap.Awake += Minimap_Awake;
-            //On.Minimap.UpdateMap += Minimap_UpdateMap;
+            On.Minimap.UpdatePlayerMarker += Minimap_UpdatePlayerMarker;
             Jotunn.Logger.LogInfo("RotateMinimapMod has loaded!");
         }
 
-        //private void Minimap_UpdateMap(On.Minimap.orig_UpdateMap orig, Minimap self, Player player, float dt, bool takeInput)
-        //{
-        //    self.m_smallZoom = 0.01f * FloatConfig.Value;
-        //}
+        private void Minimap_UpdatePlayerMarker(On.Minimap.orig_UpdatePlayerMarker orig, Minimap self, Player player, Quaternion playerRot)
+        {
+            if (self.m_mode == Minimap.MapMode.Small)
+            {
+                self.m_smallMarker.rotation = Quaternion.Euler(0, 0, 0);
+                self.m_smallShipMarker.gameObject.SetActive(value: false);
+            } else
+            {
+                
+            }
+        }
 
         private void Minimap_Awake(On.Minimap.orig_Awake orig, Minimap self)
         {
@@ -39,9 +44,8 @@ namespace RotateMinimapMod
 
         private void Minimap_CenterMap(On.Minimap.orig_CenterMap orig, Minimap self, Vector3 centerPoint)
         {
-            self.m_smallRoot.transform.Find("map").rotation = Quaternion.Euler(0, 0, Player.m_localPlayer.m_eye.transform.rotation.eulerAngles.y);
+            self.m_mapImageSmall.transform.rotation = Quaternion.Euler(0, 0, Player.m_localPlayer.m_eye.transform.rotation.eulerAngles.y);
             self.m_pinRootSmall.transform.rotation = Quaternion.Euler(0, 0, Player.m_localPlayer.m_eye.transform.rotation.eulerAngles.y-360);
-            self.m_smallMarker.transform.rotation = Quaternion.Euler(0, 0, 0);
             orig(self, centerPoint);
         }
     }
